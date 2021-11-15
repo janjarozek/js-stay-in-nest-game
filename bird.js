71037b0521ad20d97d90c661bb20e4661666cf8a
janjarozek <74878517+janjarozek@users.noreply.github.com>
2021-11-15 22:44:41 +0100

    added pause effect on Escape press and abillity to move the bird by arrow keys

--- bird.js
+++ bird.js
@@ -1,4 +1,7 @@
+const arrows = ["ArrowUp", "ArrowDown", "ArrowRight", "ArrowLeft"];
+
 const BIRD_SIZE = 35;
+const BIRD_JUMP = 30;
 const birdXcenter = (window.innerWidth - BIRD_SIZE) / 2;
 const birdYcenter = (window.innerHeight - BIRD_SIZE) / 2;
 
@@ -7,9 +10,43 @@ const bird = document.querySelector("[data-bird]");
 export function setupBird() {
     bird.style.setProperty("--bird-size", BIRD_SIZE);
     setBirdPosition(birdXcenter, birdYcenter);
+    // add event listners
+    document.removeEventListener("keyup", moveBird);
+    document.addEventListener("keyup", moveBird);
 }
 
 function setBirdPosition(x, y) {
     bird.style.setProperty("--bird-left", x);
     bird.style.setProperty("--bird-top", y);
+}
+
+export function getBirdPosition() {
+    // return bird.getBoundingClientRect();
+    let x = parseFloat(getComputedStyle(bird).getPropertyValue("--bird-left"));
+    let y = parseFloat(getComputedStyle(bird).getPropertyValue("--bird-top"));
+    return [x, y];
+}
+
+export function moveBird(event) {
+    // get bird position
+    let [x,y] = getBirdPosition();
+    // control the bird by arrow keys
+    if (arrows.includes(event.code)) {
+        // console.log("Clicked: ", event.code);
+        switch (event.code) {
+            case "ArrowUp":
+                setBirdPosition(x, y-BIRD_JUMP);
+                break;
+            case "ArrowDown":
+                setBirdPosition(x, y+BIRD_JUMP);
+                break;
+            case "ArrowLeft":
+                setBirdPosition(x-BIRD_JUMP, y);
+                break;
+            case "ArrowRight":
+                setBirdPosition(x+BIRD_JUMP, y);
+                break;
+            default: return null;
+        }
+    }
 }
--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 import { setupBird } from "./bird.js";
 
-document.addEventListener("keypress", setupGame, { once: true });
+// document.addEventListener("keyup", logKey);
+// function logKey(e) {
+//     console.log(e.code)
+// }
+document.addEventListener("keyup", startGame);
+// document.addEventListener("keyup", startGame, { once: true });
 
 const nest = document.querySelector("[data-nest]");
 const score = document.querySelector("[data-score]");
+
+var gameTime = 0;
+let gameIntervalTimer;
+let gameStarted = false;
+let gamePaused = false;
 
 let lastTime = null
 function updateLoop(currentLoopTime) {
@@ -11,17 +21,53 @@ function updateLoop(currentLoopTime) {
     if ( lastTime === null ) {
         lastTime = currentLoopTime;
         window.requestAnimationFrame(updateLoop);
+        // document.addEventListener("keyup", pauseGame, { once: true });
         return
     }
+    // cal the delta time and handle changes accordingly
     const delta = currentLoopTime - lastTime;
-    console.log(delta)
-
+    // handle game logic based on the time flow ...
+    if(gameStarted) {
+        score.innerHTML = `Your time: ${gameTime}`;
+    } else {
+        score.innerHTML = `Game paused!`;
+    }
+    // set lastTime for next iteration
     lastTime = currentLoopTime;
     window.requestAnimationFrame(updateLoop)
 }
 
-function setupGame() {
-    score.innerHTML = "Your time: ";
-    setupBird();
+function startGame(event) {
+    // if (event.code !== 'Space') return
+    if (event.code === 'Space' && gameStarted === false) {
+        gameStarted = true;
+        // reset when lost
+        // gameTime = 0;
+        score.innerHTML = `Your time: ${gameTime}`;
+        setupBird();
+        gameTimeCounter();
+    }
+    if (event.key === 'Escape' && gameStarted === true) {
+        gameStarted = false;
+        gamePause();
+    }
     window.requestAnimationFrame(updateLoop);
+}
+
+function gamePause() {
+    gameStarted = false;
+    lastTime = null;
+    clearInterval(gameIntervalTimer);
+}
+
+function gameReset() {
+    gameTime = 0;
+    gamePause();
+    document.addEventListener("keyup", startGame, { once: true });
+}
+
+function gameTimeCounter() {
+    gameIntervalTimer = setInterval(() => {
+        gameTime++;
+    }, 1000);
 }
